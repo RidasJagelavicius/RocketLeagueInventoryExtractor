4622e4e71888d23585efaf3fb781a1a57f425185
Ridas Jagelavicius <RidasJagelavicius@gmail.com>
2019-12-03 19:38:06 -0600

    Updated price scraper to grab all items

--- Rocket League Inventory Extractor/PriceScraper.py
+++ Rocket League Inventory Extractor/PriceScraper.py
@@ -13,7 +13,7 @@ def main():
     errorList = []
 
     # Store prices
-    prices = []
+    prices = {}
 
     # Open a connection to the site using HTTP requests
     print('Checking sources')
@@ -35,31 +35,30 @@ def main():
         print('Generating creature lists')
         
         # Create a Python object (dict) of all prices
-        for priceTable in pricesBs.find_all(class_='priceTable'):
-            for item in priceTable.find_all(class_='itemRow'):
-                if('emptyCell' in item['class']):
-                    continue;
-                table = {}
-                table['name'] = item['data-itemfullname'] # Grab item name
-                table['type'] = item['data-itemtype'].capitalize() # Grab item type (ex. Topper, Decal)
-                rarity_sanitized = item['data-itemrarity'][1:-1].capitalize() # Grab rarity of item (ex. Rare, Limited)
-                while('|' in rarity_sanitized):
-                    rarity_sanitized = rarity_sanitized[:rarity_sanitized.index('|')]
-                table['rarity'] = rarity_sanitized
-                painted = {}
-                for item_painted in item.find_all(class_='priceRange'):
-                    color = item_painted['class']
-                    if('invisibleColumn' in color): # Chop some junk off
-                        continue
-                    color = color[0][5:] # Chop some junk off
-                    color_price = item_painted.text 
-                    if('&hairsp-&hairsp' in color_price):
-                        color_price = color_price.replace('&hairsp-&hairsp','-') # Sanitize scraped hyphens
-                    if('\u2014' in color_price):
-                        color_price = color_price.replace('\u2014','') # Sanitize scraped hyphens
-                    painted[color] = color_price
-                table['prices'] = painted
-            prices.append(table)
+        for item in pricesBs.find_all(class_='itemRow'):
+            if('emptyCell' in item['class']):
+                continue;
+            table = {}
+            table['name'] = item['data-itemfullname'] # Grab item name
+            table['type'] = item['data-itemtype'].capitalize() # Grab item type (ex. Topper, Decal)
+            rarity_sanitized = item['data-itemrarity'][1:-1].capitalize() # Grab rarity of item (ex. Rare, Limited)
+            while('|' in rarity_sanitized):
+                rarity_sanitized = rarity_sanitized[:rarity_sanitized.index('|')]
+            table['rarity'] = rarity_sanitized
+            painted = {}
+            for item_painted in item.find_all(class_='priceRange'):
+                color = item_painted['class']
+                if('invisibleColumn' in color): # Chop some junk off
+                    continue
+                color = color[0][5:].lower() # Chop some junk off
+                color_price = item_painted.text 
+                if('&hairsp-&hairsp' in color_price):
+                    color_price = color_price.replace('&hairsp-&hairsp','-') # Sanitize scraped hyphens
+                if('\u2014' in color_price):
+                    color_price = color_price.replace('\u2014','').replace('\u2003','') # Sanitize scraped hyphens
+                painted[color] = color_price
+            table['prices'] = painted
+            prices[table['name'].replace(" ", "").lower()] = table
         print("Generated prices")  
         
     #Otherwise there are errors
